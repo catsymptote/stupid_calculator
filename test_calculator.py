683138b37886d5968268fa4c2324929c9b215a58
catsymptote <catsymptote@gmail.com>
2019-11-11 22:05:38 +0100

    Added testing with pytest

---
+++ test_calculator.py
@@ -0,0 +1,32 @@
+# Generate the calculator.
+from generator import generate
+operators = "+-*/"
+a_min = -5
+a_max = 5
+b_min = -5
+b_max = 5    
+generate(operators=operators, a_min=a_min, a_max=a_max, b_min=b_min, b_max=b_max)
+
+# Load calculator.
+from calculator import *
+
+
+def test_all_possible_combinations():
+    # Basic calculator for testing purposes.
+    def basic_calc(a, op, b):
+        if op=="+":
+            return a+b
+        elif op=="-":
+            return a-b
+        elif op=="*":
+            return a*b
+        elif op=="/":
+            if b==0:
+                return None
+            return a/b
+
+    # Iterate through all possibilities.
+    for a in range(a_min, a_max):
+        for op in operators:
+            for b in range(b_min, b_max):
+                assert calc(a, op, b) == basic_calc(a, op, b)
