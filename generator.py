e4f577e8541ab996ce568b731ef4d6c2277f85ce
catsymptote <catsymptote@gmail.com>
2019-11-11 22:05:50 +0100

    Updated generator

--- generator.py
+++ generator.py
@@ -1,26 +1,25 @@
-operators = "+-*/"
-a_max = 50
-b_max = 50
-operators = "+-*/"
+def write_calc(operators, a_min, a_max, b_min, b_max):
+    calc_str = ""
+    #calc_str += 'def user_input():\n'
+    #calc_str += '\ta = int(input("a> "))\n'
+    #calc_str += '\top = input("operator> ")[0]\n'
+    #calc_str += '\tb = int(input("b> "))\n'
+    #calc_str += '\treturn a, op, b\n\n\n'
+    
+    calc_str += "def calc(a, op, b):\n"
+    calc_str += '\tif op=="/" and b==0:\n'
+    calc_str += '\t\treturn None\n'
+    for op in operators:
+        for a in range(a_min, a_max):
+            for b in range(b_min, b_max):
+                if not(op=="/" and b==0):
+                    calc_str += '\tif a=={0} and op=="{1}" and b=={2}:\n'.format(a, op, b)
+                    calc_str += '\t\treturn {0}{1}{2}\n'.format(a, op, b)
 
 
-calc_str = 'a = int(input("a> "))\n'
-calc_str += 'op = input("operator> ")[0]\n'
-calc_str += 'b = int(input("b> "))\n\n'
+    f = open('calculator.py', 'w')
+    f.write(calc_str)
+    f.close()
 
-#calc_str += 'if op=="/" and b==0:\n'
-#calc_str += '\tprint("Dividing by 0 is bad, mkay!")\n'
-
-for op in operators:
-    for a in range(a_max):
-        for b in range(b_max):
-            if not(op=="/" and b==0):
-                calc_str += 'if a=={0} and op=="{1}" and b=={2}:\n'.format(a, op, b)
-                calc_str += '\tprint({0}{1}{2})\n'.format(a, op, b)
-
-#calc_str += 'else:\n'
-#calc_str += '\tprint("Invalid input .. (probably.)")\n'
-
-f = open('calculator.py', 'w')
-f.write(calc_str)
-f.close()
+def generate(operators="+-*/", a_min=-10, a_max=10, b_min=-10, b_max=10):
+    write_calc(operators, a_min, a_max, b_min, b_max)
